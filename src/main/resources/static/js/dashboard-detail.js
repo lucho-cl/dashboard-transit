440afe1aeb90b436b14919ac4ba735711469c4d3
lucho <luis.sepulveda.a@gmail.com>
2018-11-13 00:44:56 -0300

    se le da funcionalidad al combo de regiones tanto en el home como en el detalle, además se cambia la manera de navegar (con el commit anterior)

--- src/main/resources/static/js/dashboard-detail.js
+++ src/main/resources/static/js/dashboard-detail.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
 				
 	        });
 			$('#regionSelect').html(regionSelect);
+			$("#regionSelect").val($("#regionSeleccionada").val())
 		},
 		error : function(e) {
 			alert("ERROR: ", e);
@@ -20,9 +21,33 @@ $(document).ready(function() {
 
 
 			$('#regionSelect').change(function() {
-				alert("ir al detalle del grafico 1 y region "+$("#regionSelect").val());
+//				alert("ir al detalle del grafico 1 y region "+$("#regionSelect").val());
 //				ir al detalle de la region seleccionada con el reporte 1
+				change_detail($("#regionSelect").val(),1);
 //				getReportes();
 			});
-	
+	function change_detail(idRegion, idReporte){
+		window.location.href = 'detail?idCuadro='+idReporte+'&idRegion='+idRegion;
+		
+//		var url = 'detail?idCuadro='+idReporte+'&idRegion='+idRegion
+//		$.get(url,function(data, status){
+//			console.log('${data}');
+//		});
+
+//		$.ajax({
+//			type : "GET",
+//			url : "detail",
+//			data : {
+//				'idCuadro' : idReporte
+//				,'idRegion': idRegion
+//			},
+//			success: function(result){
+//				alert("EXITO!");
+//				console.log(result);
+//			},
+//			error : function(e) {
+//				console.log("ERROR: ", e);
+//			}
+//		});
+	}
 });
